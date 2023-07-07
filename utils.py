30c6c4fa6e58f4a439c8c6c232369ddf293aabd1
Vicente Cser <vicentecser@gmail.com>
2023-07-07 19:04:50 -0400

    Fix bugs (now working almost correctly)

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ from Tetris.tetris import Tetris
 from copy import deepcopy
 from Tetris.global_variables import LEFT_KEY, RIGHT_KEY, DOWN_KEY, PULL_DOWN_KEY, ROTATE_KEY
 import random
+import time
 
 def clear_rows(t: Tetris):
     # set initial number of cleared lines as 0
@@ -27,9 +28,11 @@ def calculate_possible_moves(t: Tetris):
     piece = deepcopy(t.current_piece)
     shape = piece.shape
     moves = {}
+    prevscore = t.score
     
     # por cada rotacion de la pieza
-    for sh in range(len(shape)):
+    for sh in range(0, len(shape)):
+        print("rotacion =", sh)
         # obtener ancho de la pieza
         X = [i for (i, _) in piece.get_formatted_shape()]
         width = max(X) - min(X)
@@ -37,7 +40,8 @@ def calculate_possible_moves(t: Tetris):
         piece.rotation = sh
         left = 2 - piece.get_left_bound()
         # iterar desde izquierda a derecha
-        for i in range(left, 10 - width + 1):
+        for i in range(0, 10):
+            piece.rotation = 0
             actions = [ROTATE_KEY] * sh
             # crear una copia del juego para hacer la simulacion
             tetris_copy = Tetris()
@@ -45,8 +49,12 @@ def calculate_possible_moves(t: Tetris):
             tetris_copy.grid = deepcopy(t.grid)
             tetris_copy.current_piece = piece
             tetris_copy.next_piece = piece
+            
             piece.y = 0
-            piece.x = i
+            piece.x = 5
+
+            if (not piece.in_valid_space(tetris_copy.grid)):
+                continue
             # calcular movimientos necesarios (action = left || right)
             if i < 5:
                 actions += [LEFT_KEY] * (5-i)
@@ -56,18 +64,16 @@ def calculate_possible_moves(t: Tetris):
             actions += [PULL_DOWN_KEY]
             for action in actions:
                 tetris_copy.play_game(action)
-                tetris_copy.current_piece = piece
-                tetris_copy.next_piece = piece
+                # tetris_copy.current_piece = piece
+                # tetris_copy.next_piece = piece
 
             # calcular puntaje generado por las acciones anteriores
             # score = tetris_copy.score
             # grid = tetris_copy.set_grid()
             # calcular altura
-            while piece.in_valid_space(t.set_grid()):
-                piece.y += 1
-            # since in valid space is violated now, so fix y coordinate by shifting one block up
-            piece.y -= 1
             altura = 20 - piece.y
+            altura = 0 if altura < 0 else altura
+            print("altura = ", altura)
             # altura = altura if altura > 0 else 0
             filas = clear_rows(tetris_copy)
             transiciones_filas = random.randint(0, 6)
@@ -79,7 +85,8 @@ def calculate_possible_moves(t: Tetris):
             score = altura*A[0] + filas*A[1] #+ transiciones_filas*A[2] + transiciones_columnas*A[3] + huecos*A[4] + pozos*A[5]
 
             moves[score] = actions
-            print(score, moves[score], "ALTURA = ", altura)
-
+            # time.sleep(1)
+            # print(score, moves[score], "ALTURA = ", altura)
+    t.score = prevscore
     print("MAX = ", max(moves.keys()))
     return moves[max(moves.keys())]
