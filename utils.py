edcb0f42ddeabe65e82fd3448da81f97c28dfddb
Vicente Rodriguez Alfaro <87551025+el-vigesimo-vicho@users.noreply.github.com>
2023-07-09 18:55:34 -0400

    params, hay bug de posicion

--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ from Tetris.global_variables import LEFT_KEY, RIGHT_KEY, DOWN_KEY, PULL_DOWN_KEY
 import random
 import time
 
+
+#calcular betta
 def clear_rows(t: Tetris):
     # set initial number of cleared lines as 0
     num_cleared_rows = 0
@@ -22,6 +24,66 @@ def calculate_Tfilas():
 
 def calculate_Tcolumnas():
     pass
+
+
+#calcular gamma
+def calculate_Trans_rows(t:Tetris):
+    matrix = t.get_grid_state()
+    contador=0
+    for i in range(len(matrix)):
+        for j in range(0,len(matrix[i])-1):
+                if((matrix[i][j]==0 and matrix[i][j+1]!=0) or (matrix[i][j]!=0 and matrix[i][j+1]==0)):
+                    contador+=1
+    return contador
+
+
+
+#calcular delta
+def calculate_Trans_columns(t:Tetris):
+    matrix = t.get_grid_state()
+    contador=0
+
+    for i in range(len(matrix[0])):# matrix de 0 debido a que todas filas tienen la misma cantidad de bloques
+        for j in range(0,len(matrix)-1):
+                if((matrix[j][i]==0 and matrix[j+1][i]!=0) or (matrix[j][i]!=0 and matrix[j+1][i]==0)):
+                    contador+=1
+    return contador
+
+#calcular zeta
+def calculate_num_pozos(t:Tetris):
+    matrix = t.get_grid_state()
+    num_pozos=0
+    es_Pozo=0
+    
+    for i in range(1,len(matrix[0])-1):
+        for j in range(len(matrix)):
+            if(matrix[j][i]==0):
+                if(matrix[j][i-1]!=0 and matrix[j][i+1]!=0):
+                        es_Pozo+=1
+            else:
+                if(es_Pozo>=2):
+                    num_pozos+=1
+                es_Pozo=0
+    return num_pozos
+
+#calcular epsylon            
+def calculate_num_hol(t:Tetris):
+    matrix=t.get_grid_state()
+    contador=0
+    for i in range(1,len(matrix)):
+        for j in range(len(matrix[i])):
+            if(matrix[i][j]==0 and matrix[i-1][j]!=0):
+                contador+=1                
+    return contador
+
+#calcular alfa x
+def calculate_highest_peak(t:Tetris,altura):
+    matrix = t.get_grid_state()
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if(matrix[i][j]!=0):
+                return(len(matrix)-i)
+    return 0
 
 
 def calculate_possible_moves(t: Tetris):
@@ -70,10 +132,19 @@ def calculate_possible_moves(t: Tetris):
             # score = tetris_copy.score
             # grid = tetris_copy.set_grid()
             # calcular altura
+            calculate_Trans_columns(tetris_copy)
             altura = 20 - piece.y
             altura = 0 if altura < 0 else altura
             # print("altura = ", altura)
-            # altura = altura if altura > 0 else 0
+            alfa = calculate_highest_peak(tetris_copy,altura)
+            alfa = 0 if alfa < 0 else alfa
+            beta = clear_rows(tetris_copy)
+            gamma = calculate_Trans_rows(tetris_copy)
+            delta = calculate_Trans_columns(tetris_copy)
+            epsylon = calculate_num_hol(tetris_copy)
+            zetta = calculate_num_pozos(tetris_copy)
+            #print("num pozo es: ",zetta)
+
             filas = clear_rows(tetris_copy)
             transiciones_filas = random.randint(0, 6)
             transiciones_columnas = random.randint(0, 8)
@@ -81,11 +152,14 @@ def calculate_possible_moves(t: Tetris):
             pozos = random.randint(0, 8)
 
             A = (-4.500158825082766, 3.4181268101392694, -3.2178882868487753, -9.348695305445199, -7.899265427351652, -3.3855972247263626)
-            score = altura*A[0] + filas*A[1] #+ transiciones_filas*A[2] + transiciones_columnas*A[3] + huecos*A[4] + pozos*A[5]
+            
+            score = alfa*A[0] + beta*A[1] + gamma*A[2] + delta*A[3] + epsylon*A[4] + zetta*A[5]
 
             moves[score] = actions
             # time.sleep(1)
-            # print(score, moves[score], "ALTURA = ", altura)
+            #print("ALTURA = ", altura)
+            print ("score es",score)
     t.score = prevscore
     print("MAX = ", max(moves.keys()))
     return moves[max(moves.keys())]
+
